961040df7170788ab607b9a3719375d1a4bec122
Ömer Faruk Özkan <omerfarukozkaan@gmail.com>
2025-05-06 16:23:57 +0300

    initial commit

---
+++ backend/src/routes/chat.route.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getStreamToken } from "../controllers/chat.controller.js";
+
+const router = Router();
+
+router.get("/token", protectRoute, getStreamToken);
+
+export default router;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.jsx";
+
+import "./index.css";
+import "stream-chat-react/dist/css/v2/index.css";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
